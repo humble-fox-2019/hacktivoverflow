f10e1c312eeedf150c40fdd847217a781172a152
TigorLazuardi <tigor.hutasuhut@gmail.com>
2019-09-26 18:00:01 +0700

    axios

--- underflow/src/config/axios.js
+++ underflow/src/config/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export default axios.create({
-    baseURL: process.env.HOST_URL || 'http://localhost:3000',
+    baseURL: process.env.VUE_APP_HOST_URL || 'http://localhost:3000',
     timeout: 1000,
     headers: { token: localStorage.getItem('token') }
 })
