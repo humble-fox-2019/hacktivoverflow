568298f6345bb80adef676f073c184dfb265ee70
Josprima Sihombing <josprima.id@gmail.com>
2019-09-26 18:33:19 +0700

    Josprima Sihombing

--- server/config/mongoose.js
+++ server/config/mongoose.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-const MONGO_URI = 'mongodb://localhost:27017/HacktivOverFlow'
+const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/HacktivOverFlow'
 
 
 mongoose.connect(MONGO_URI, {
