94df681d835ea901a83501ea4c2b9cd953c44b1e
das <ardynn.d@gmail.com>
2019-09-27 10:50:23 +0700

    Firdaus Sugiarto Ardi Pratama

--- server/routes/answer.js
+++ server/routes/answer.js
@@ -4,10 +4,7 @@ const { Authentication, AuthorizationAnswer } = require('../middlewares/authenti
 
 router.use(Authentication);
 router.get('/:id', answer.readAnswer);
-router.get('/edit/:id', AuthorizationAnswer, answer.readOne);
 router.post('/:id', answer.create);
 router.patch('/vote/:id', answer.vote);
-router.patch('/edit/:id', AuthorizationAnswer, answer.edit);
-router.delete('/:id', AuthorizationAnswer, answer.delete);
 
 module.exports = router;
