213a80c07b5afc20aa2ff5f5ee7637094b44bce4
guntoroyk <guntoro.gyk@gmail.com>
2020-05-28 17:03:20 +0700

    ready to deploy

--- client/src/configs/axios.js
+++ client/src/configs/axios.js
@@ -2,7 +2,8 @@ import axios from 'axios'
 
 const myaxios = axios.create({
   // baseURL: 'http://localhost:3000'
-  baseURL: 'http://api.toooverflow.guntoroyk.site'
+  // baseURL: 'http://api.toooverflow.guntoroyk.site'
+  baseURL: 'https://toooverflow-server.herokuapp.com/'
 })
 
 export default myaxios
